df78ca9e908409616c32d2614843521560b3ce60
juaoantonio <joaobarbosadev@gmail.com>
2022-02-01 21:25:35 -0300

    Ask to user the file path

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import json
+import os
 import notion
 import csv_manipulator
-from decouple import config
+from tkinter import filedialog
 from data_hoje import hoje
 
 # Armazena o dia presente por padrão, mas pode configurar com o dia que quiser, basta copiar na váriavel dia_atual
@@ -13,11 +15,26 @@ from data_hoje import hoje
 # 'Sábado'
 dia_atual = hoje()
 
+if not os.path.isfile('settings.json'):
+    excel_path = filedialog.askopenfilename()
+
+    config = {
+        'excel_path': excel_path,
+        'token': 'secret_TG7Xp8dj3VsNcm2FMskZFlzEdvJMmvoOMXmt81OGk42'
+    }
+
+    json_object = json.dumps(config, indent=4)
+    with open('settings.json', 'w') as outfile:
+        outfile.write(json_object)
+
+with open('settings.json', 'r') as json_file:
+    valores = json.load(json_file)
+
 # Caminho da planilha de análises
-excel_path = r'/home/jaab/Desktop/relatorio/analise.xlsm'
+excel_path = valores['excel_path']
 
 # Token secreto de acesso do Notion
-token_integracao = config('TOKEN')
+token_integracao = valores['token']
 
 # Pages ID de cada vendedor
 pages_id = [
